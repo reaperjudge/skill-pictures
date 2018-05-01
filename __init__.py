a7a59b50d129d831fc06c5bffacae615d36ec34d
reaperjudge <38620064+reaperjudge@users.noreply.github.com>
2018-05-01 17:06:28 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -20,7 +20,7 @@ class Backtalk(MycroftSkill):
         
     def handle_shutdown(self, message):
         # Sending a command to mycroft, speak Greetings Dialog
-        subprocess.call(["sudo","shutdown","-h","now"])
+        subprocess.call(["sudo","poweroff"])
         self.speak_dialog("respond")
         
     def stop(self):
